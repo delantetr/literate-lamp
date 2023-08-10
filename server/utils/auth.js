08e92ad047b5bd4196c9c879e518ff030d4e0025
Delante Randolph <delantetr@MacBook-Pro.local>
2023-08-10 11:21:44 -0400

    initial commit

---
+++ server/utils/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecretssshhhhhhh';
+const expiration = '2h';
+
+module.exports = {
+  authMiddleware: function ({ req }) {
+    
+
+
+
+  },
+
+
+  
+  signToken: function ({ email, username, _id }) {
+   
+
+
+
+  },
+};
