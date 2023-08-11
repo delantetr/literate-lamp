e50bb4aae443d6c9cd85e73e0e15348e2ed8af92
Reyyahs <133382184+Reyyahs@users.noreply.github.com>
2023-08-11 10:09:56 -0400

    added npm i bootstrap bootstrap and create a home.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,10 @@ import {
   InMemoryCache,
   ApolloProvider,
 } from '@apollo/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ExampleComponent from './pages/ExampleComponent';
+import { BrowserRouter as Router, Routes, Route, Link, useLinkClickHandler } from 'react-router-dom';
+import Home from './Components/Home';
+import {Navbar,Nav,Container} from 'react-bootstrap';
+
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -18,12 +20,21 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
+        <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme='dark' >
+            <Container>
+              <Navbar.Brand as={Link} to={'/Home'}>PrepPal</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link as={Link} to='/Home'>Home</Nav.Link>
+                  <Nav.Link as={Link} to='/Profile' >Profile</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
           <div className="container">
             <Routes>
-              <Route 
-                path="/"
-                element={<ExampleComponent />}
-              />
+              <Route path="/Home" element={<Home/>}/>
             </Routes>
           </div>
         </div>
