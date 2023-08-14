5b09bb6412f0f016ac525f10bc893b6b6010a847
Reyyahs <133382184+Reyyahs@users.noreply.github.com>
2023-08-14 12:41:44 -0400

    added functionality to signup page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,30 +3,37 @@ import { Button, Form, Card } from 'react-bootstrap';
 
 const Login = () => {
   return (
-    <Card style={{ width: '18rem' }}>
-    <Card.Body>
-      <Card.Title>Login</Card.Title>
-      <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+    <main className='flex-row justify-center mb-4'>
+      <div className='mt-5'>
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            {/* Card Title */}
+            <Card.Title>Login</Card.Title>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
+            {/* Login Form */}
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                {/* You can add any additional text or labels here */}
+              </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-    </Card.Body>
-  </Card>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
 
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+      </div>
+    </main>
   );
 }
 
