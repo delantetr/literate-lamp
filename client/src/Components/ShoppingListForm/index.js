6de678488c0ef9680c493a95992ca2a4e11bcd62
kingpin <monique.ahlheim@live.com>
2023-08-16 13:48:03 -0400

    fixed functions in shopping list pages and components

--- client/src/Components/ShoppingListForm/index.js
+++ client/src/Components/ShoppingListForm/index.js
@@ -1,55 +1,86 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
 
-function ShoppingListForm(props) {
-  const [input, setInput] = useState('');
+import { ADD_TO_SHOPPING_LIST } from '../../utils/mutations';
+import { QUERY_ME } from '../../utils/queries';
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    props.onSubmit({
-      id: Math.random(Math.floor() * 1000),
-      text: input,
+function ShoppingListForm() {
+    const [formState, setFormState] = useState({
+      userId: '',
+      items: '',
     });
+  
+    const [addToShoppingList, { error }] = useMutation(ADD_TO_SHOPPING_LIST, {
+      update(cache, { data: { addToShoppingList } }) {
+        try {
+          const { me } = cache.readQuery({ query: QUERY_ME });
+  
+          cache.writeQuery({
+            query: QUERY_ME,
+            data: { me: [addToShoppingList, ...me] },
+          });
+        } catch (e) {
+          console.error(e);
+        }
+      },
+    });
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      console.log(event);
+      try {
+        const { data } = await addToShoppingList({
+          variables: { ...formState },
+        });
+  
+        setFormState({
+          userId: '',
+          items: '',
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+  
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+  
+      if (name === 'text') {
+        setFormState({ ...formState, [name]: value });
+      } else if (name !== 'text') {
+        setFormState({ ...formState, [name]: value });
+      }
+    };
 
-    setInput('');
-  };
-
-  const handleChange = (e) => {
-    setInput(e.target.value);
-  };
-
-  // First we check to see if "edit" prop exists. If not, we render the normal form
-  // If the prop "edit" exists, we know to render the update form instead
-  return !props.edit ? (
+  return (
     <div>
-      <form className="list-form" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Add an item to your shopping list"
-          value={input}
-          name="text"
-          className="list-input"
-          onChange={handleChange}
-        ></input>
-        <button className="list-button">Add item</button>
-      </form>
-    </div>
-  ) : (
-    <div>
-      <h3>Update entry: {props.edit.value}</h3>
-      <form className="list-form" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder={props.edit.value}
-          value={input}
-          name="text"
-          className="list-input"
-          onChange={handleChange}
-        ></input>
-        <button className="list-button">Update</button>
+      <form
+        className="flex-row justify-center justify-space-between-md align-center"
+        onSubmit={handleSubmit}
+      >
+        <div className="col-12">
+          <textarea
+            name="text"
+            placeholder=""
+            value={formState.shoppingList}
+            className="form-input w-100"
+            style={{ lineHeight: '1.5' }}
+            onChange={handleChange}
+          ></textarea>
+        </div>
+        <div className="col-12 col-lg-3">
+          <button className="btn btn-info btn-block py-3" type="submit">
+            Add Item
+          </button>
+        </div>
+        {error && (
+          <div className="col-12 my-3 bg-danger text-white p-3">
+            Something went wrong...
+          </div>
+        )}
       </form>
     </div>
   );
-}
+};
 
 export default ShoppingListForm;
